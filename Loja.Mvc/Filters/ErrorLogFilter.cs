ff041c59b0b4652ac7ce7a3ed233e85f28a4a434
Thiago <thiagomachado@cginformaticasp.com.br>
2018-02-07 20:54:37 -0200

    Aula 1 - area mvc + log

---
+++ Loja.Mvc/Filters/ErrorLogFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loja.Mvc.Filters
+{
+    public class ErrorLogFilter: HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+
+
+            if (filterContext != null && filterContext.Exception != null)
+            {
+
+                
+                var controller = filterContext.RouteData.Values["controller"].ToString();
+                var action = filterContext.RouteData.Values["action"].ToString();
+                var loggerName = $"{controller}Controller.{action}";
+
+                //loggerName = loggerName.Concat("aaa");
+
+                log4net.LogManager.GetLogger(loggerName).Error(filterContext.Exception.Message, filterContext.Exception);
+                base.OnException(filterContext);
+
+            }
+        }
+    }
+}
